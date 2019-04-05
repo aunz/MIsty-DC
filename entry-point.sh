7ba6c8944a138b9e88f7d2fa4c6c3f0657e42248
aunz <aunz10@yahoo.com>
2019-04-05 10:48:19 -0400

    re order server

--- entry-point.sh
+++ entry-point.sh
@@ -1,4 +1,12 @@
-#!/bin/sh
-node node/index &
+#!/bin/bash
+
+# turn on bash's job control
+set -m
+
+# run python and put it in the background
 cd /app/python
 python3 server.py &
+
+# now run node
+cd /app/node
+node index 
